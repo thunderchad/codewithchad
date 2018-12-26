3876b23abc3d215075b4cb63ed6ad981a9b069e7
Chad Thundercock <thunderchad@protonmail.com>
2018-12-26 22:13:23 +0200

    blow a hot chat dev load all over this git

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-// copied from some
-$(document)
+
+$(document )// doesn't even work lmao
     .one('focus.autoExpand', 'textarea.expand-like-chads-cock', function(){
         var savedValue = this.value;
         this.value = '';
@@ -13,56 +13,105 @@ $(document)
         this.rows = minRows + rows;
     });
 
+// NOTE FOR VIRGINS: work on this
+
 var tasks = [
     {
-        descr: "print hello world",
-        output: [
-            "hello world"
-        ],
-        examples: [
-            'print("what the fuck");',
-            'print("brah");'
-        ],
-        defaultCode: ""
+        descr: "print hello chad",
+        dataGen: () => "",
+        dataToHeader: g => g,
+        outputValidator: (g, o) => o == "hello chad",
+        inputValidator: (g, i) => true,
+        examples:
+            `print("what the fuck")
+             print("brah")`,
+        defaultCode: "write your code here",
+        hints: "fuck you"
+    },
+    {
+        descr: "make a variable with 10 as the value and print it",
+        examples:
+            `var chadsDicklength = 9999
+             print(chadsDicklength)`,
+        dataGen: () => "",
+        dataToHeader: g => "var chadsDicklength = 9999",
+        outputValidator: (g, o) => o == 10,
+        inputValidator: (g, i) => i.includes("var"),
+        defaultCode: "i love chad",
+        hints: "what the fuck do you need help for"
+    },
+    {
+        descr: "if x is bigger than 5, then print chad is king. otherwise print chad is lord.",
+        examples:
+        `var chadsGirth = 9999
+         if (chadsGirth < 1000) {
+             print("impossible!")
+         }
+         else {
+             print("chad is big and ready for you to please him.")
+         }`,
+        dataGen: () => (Math.floor(Math.random() * 10)),
+        dataToHeader: g => `
+            var chadsGirth = 9999
+            var x = ` + g,
+        outputValidator: (g, o) => {
+            if (g > 5) {
+                return o == "chad is king"
+            }
+            else {
+                return o == "chad is lord"
+            }
+        },
+        inputValidator: (g, i) => i.includes("x") && i.includes("if") && i.includes("else"),
+        defaultCode: "chad is teaching me so well",
+        hints: "Chad thinks about the math teacher he banged. > = >= < <= if you know what he means."
     }
 ];
 
-var output = [];
+var outputLines = [];
+var output = "";
 var goodCount = 0;
 var taskIdx = 0;
 var curTask = tasks[taskIdx];
 
 function print(x) {
     console.log(x);
-    output.push(x);
+    if (output == "") {
+        output = x;
+    }
+    else {
+        output = output + "\n" + x;
+    }
+    outputLines.push(x);
 };
 
 // returns true if chad approves
 function executeCode(code) {
-    output = [];
+    output = "";
+    outputLines = [];
+    var g = curTask.dataGen();
+    var isError = false;
     try {
-        eval(code); 
+        var inputCode = curTask.dataToHeader(g);
+        var allCode = inputCode + "\n" + code;
+        eval(allCode); 
     } catch (e) {
         console.log("you fucked up and broke it somehow");
         console.log(e.message);
+        isError = true;
         if (e instanceof SyntaxError) {
-            output.push(e.message);
+            outputLines.push(e.message);
         }
     }
     var opdiv = $('#code-output');
     opdiv.empty();
-    for (var i = 0; i < output.length; i++) {
-        opdiv.append("<p>" + output[i] + "</p>");
+    for (var i = 0; i < outputLines.length; i++) {
+        opdiv.append("<p>" + outputLines[i] + "</p>");
     }
-    if (output.length != curTask.output.length) {
+    if (isError) {
         return false;
     }
-    for (var i = 0; i < output.length; i++) {
-        if (output[i] != curTask.output[i]) {
-            return false;
-        }
-    }
-    return true;
+    return curTask.inputValidator(g, code) && curTask.outputValidator(g, output);
 }
 
 function updateChallenge() {
@@ -72,25 +121,18 @@ function updateChallenge() {
     $("#descr").text(t.descr);
     var exdiv = $("#examples");
     exdiv.empty();
-    for (var i = 0; i < t.examples.length; i++) {
-        exdiv.append("<p>" + t.examples[i] + "</p>");
+    var opdiv = $('#code-output');
+    opdiv.empty();
+    var exlines = t.examples.split('\n');
+    for (var i = 0; i < exlines.length; i++) {
+        exdiv.append("<p>" + exlines[i] + "</p>");
     }
+    $('#hints').text(t.hints);
+    document.getElementById('code-input').value = t.defaultCode;
 }
 
 function startChallenge() {
     updateChallenge();
-}
-
-function chadsButton() {
-    var code = document.getElementById('code-input').value;
-    var res = executeCode(code);
-    if (res) {
-        taskIdx += 1;
-        if (taskIdx >= tasks.length) {
-            showJoinChadsFraternity();
-        }
-        curTask = tasks[taskIdx];
-    }
 }
 
 
@@ -110,9 +152,31 @@ function unlockChadsCastle() {
     $("#chads-castle").removeClass("hidden");
     startChallenge();
 }
+
+function unlockChadsGarden() {
+    $("#fitness-test").addClass("hidden");
+    $("#chads-castle").addClass("hidden");
+    $("#banned").addClass("hidden");
+    $("#chads-garden").removeClass("hidden");
+    startChallenge();
+}
+function chadsButton() {
+    var code = document.getElementById('code-input').value;
+    var res = executeCode(code);
+    if (res) {
+        taskIdx += 1;
+        if (taskIdx >= tasks.length) {
+            unlockChadsGarden();
+        }
+        else {
+            curTask = tasks[taskIdx];
+            updateChallenge();
+        }
+    }
+}
+
     
 $(document).ready(function () {
-    document.cookie = "username=12313; expires=Thu, 20 Jan 20 12:00:00 EST";;
     try {
         var un = document.cookie.split(';')[0].split('=')[1];
         if (un == "virgin") {
